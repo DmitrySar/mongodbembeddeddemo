3a6a132254d21dcc7030b8f8104aad1df009290d
DMITRIY YUSHIN <49056603+DmitrySar@users.noreply.github.com>
2022-08-08 14:17:32 +0300

    Initial commit

---
+++ src/main/java/com/example/mongodbembedded/MongodbembeddedApplication.java
@@ -0,0 +1,13 @@
+package com.example.mongodbembedded;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbembeddedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MongodbembeddedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mongodbembedded/MongodbembeddedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mongodbembedded;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongodbembeddedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
